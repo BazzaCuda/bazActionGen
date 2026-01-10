cd2bc18103ca94dfbf27f26270681ea1f3168c10
Baz Cuda <BazzaCuda@users.noreply.github.com>
2026-01-10 13:08:41 +0000

    add copyright notice

--- bazActionGen.dpr
+++ bazActionGen.dpr
@@ -1,5 +1,24 @@
 program bazActionGen;
 
+{   bazActionGen
+    Copyright (C) 2021-2099 Baz Cuda
+    https://github.com/BazzaCuda/bazActionGen
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
+}
+
 {$APPTYPE CONSOLE}
 
 {$R *.res}
@@ -25,6 +44,28 @@ var
 procedure writeUnitHeader(const aSL: TStringList);
 begin
   aSL.add('unit bazAction;');
+
+  aSL.add('''
+{
+    Copyright (C) 2021-2099 Baz Cuda
+    https://github.com/BazzaCuda/
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
+
+''' + '}');
+
   aSL.add('');
   aSL.add('interface');
   aSL.add('');
