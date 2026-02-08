a678186034bce21c52fa5dfb2a5a5a49f261d45c
Baz Cuda <BazzaCuda@users.noreply.github.com>
2026-02-08 11:44:06 +0000

    create bazFuncDefs

---
+++ Win64/Debug/bazRTL.pas
@@ -0,0 +1,33 @@
+unit bazRTL;
+
+interface
+
+uses
+//  system.classes,
+//  system.generics.collections,
+//  system.ioUtils,
+  system.sysUtils;
+//  system.types;
+
+type
+  // system.sysUtils
+//  TFunc<T>          = reference to function: T;
+//  TFunc<T,TResult>  = reference to function (Arg1: T): TResult;
+//  TProc             = system.sysUtils.TProc; //   reference to procedure;
+//  TProc<T>          = reference to procedure (Arg1: T);
+//
+//  TList<T> = class(system.generics.collections.TList<T>)
+//  end;
+//
+//  TComponent        = system.classes.TComponent;
+//  TEncoding         = system.sysUtils.TEncoding;
+//  TFile             = system.ioUtils.TFile;
+//  TShiftState       = system.classes.TShiftState;
+//  TStringList       = system.classes.TStringList;
+//  EAccessViolation  = system.sysUtils.EAccessViolation;
+//  EInvalidPointer   = system.sysUtils.EInvalidPointer;
+  exception         = system.sysUtils.Exception;
+
+implementation
+
+end.
